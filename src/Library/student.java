830779f2cda97c0db2cde8934727978e1fdbcaf9
MohithNakka <86645563+mohith1976@users.noreply.github.com>
2025-09-06 19:25:49 +0530

    Initialized Student side part by developing registering student in student.java

---
+++ src/Library/student.java
@@ -0,0 +1,19 @@
+package Library;
+import java.util.*;
+ public class student{
+     String studentName;
+     String regNum;
+
+     book[] borrowedBooks = new book[3];
+
+     Scanner sc = new Scanner(System.in);
+
+     public student()
+     {
+         System.out.println("Enter Name");
+         this.studentName = sc.nextLine();
+
+         System.out.println("Enter register number");
+         this.regNum = sc.nextLine();
+     }
+ }

83c26f82cd6d5b95ade5c93c7bdf29eb6ccb8bb6
MohithNakka <86645563+mohith1976@users.noreply.github.com>
2025-09-06 19:48:19 +0530

    Finished building the basic level of  Library Management System using java .

--- src/Library/Library.java
+++ src/Library/Library.java
@@ -1,9 +1,104 @@
 package Library;
+import java.util.*;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Library {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+
+        System.out.println(
+                "********** Welcome to the Library!**********");
+        System.out.println(
+                "                  Select From The Following Options:               ");
+        System.out.println(
+                "**********************************************************************");
+
+
+        books ob = new books();
+        students obStudent = new students();
+
+        int choice;
+        int searchChoice;
+
+
+        do {
+
+            ob.dispMenu();
+            choice = sc.nextInt();
+
+
+            switch (choice) {
+
+
+                case 1:
+                    book b = new book();
+                    ob.addBook(b);
+                    break;
+
+
+                case 2:
+                    ob.upgradeBookQty();
+                    break;
+
+
+                case 3:
+
+                    System.out.println(
+                            " press 1 to Search with Book Serial No.");
+                    System.out.println(
+                            " Press 2 to Search with Book's Author Name.");
+                    searchChoice = sc.nextInt();
+
+
+                    switch (searchChoice) {
+
+
+                        case 1:
+                            ob.searchBySno();
+                            break;
+
+
+                        case 2:
+                            ob.searchByAuthorName();
+                    }
+                    break;
+
+
+                case 4:
+                    ob.showAllBooks();
+                    break;
+
+
+                case 5:
+                    student s = new student();
+                    obStudent.addStudent(s);
+                    break;
+
+
+                case 6:
+                    obStudent.showAllStudents();
+                    break;
+
+
+                case 7:
+                    obStudent.checkOutBook(ob);
+                    break;
+
+
+                case 8:
+                    obStudent.checkInBook(ob);
+                    break;
+
+
+                default:
+
+
+                    System.out.println("ENTER BETWEEN 0 TO 8.");
+            }
+
+        }
+
+
+        while (choice != 0);
     }
 }
